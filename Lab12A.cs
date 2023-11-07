54238ca392f8a20c5ecf54415771d46916514d84
Sparsegull2 <142937470+Sparsegull2@users.noreply.github.com>
2023-11-07 17:49:55 -0500

    Add files via upload

---
+++ Lab12A.cs
@@ -0,0 +1,60 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: Lab12A
+*/
+
+using System;
+class chair
+{
+    public int numOfLegs;
+    public bool rolling;
+    public string material;
+
+    public chair (int numOfLegs,bool rolling,string material)
+    {
+        this.numOfLegs = numOfLegs;
+        this.rolling = rolling;
+        this.material = material;
+    }
+}
+class Lab12A
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("You are about to create a chair.");
+        Console.Write("How many legs does your chair have: ");
+        int numlegs=int.Parse(Console.ReadLine());
+        Console.Write("Is your chair rolling (true/false): ");
+        bool rolling= bool.Parse(Console.ReadLine());
+        Console.Write("What is your chair made of: ");
+        string material= Console.ReadLine();
+        chair c1=new chair(numlegs,rolling,material);
+        
+        if (c1.rolling)
+        {
+            Console.WriteLine("\nYour chair has " + c1.numOfLegs + " legs, is rolling, and is made of " + c1.material + ".");
+        }
+        else
+        {
+            Console.WriteLine("\nYour chair has " + c1.numOfLegs + " legs, is not rolling, and is made of " + c1.material + ".");
+        }
+        Console.WriteLine("\nThis program is going to change that.");
+        c1.rolling = false;
+        c1.material = "wood";
+        c1.numOfLegs = 4;
+        if (c1.rolling)
+        {
+            Console.WriteLine("\nYour chair has " + c1.numOfLegs + " legs, is rolling, and is made of " + c1.material + ".");
+        }
+        else
+        {
+            Console.WriteLine("\nYour chair has " + c1.numOfLegs + " legs, is not rolling, and is made of " + c1.material + ".");
+        }
+
+    }
+}
---
+++ Lab12B.cs
@@ -0,0 +1,71 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: Lab12B
+*/
+
+using System;
+
+class Dog
+{
+    public int age;
+    public double weight;
+    public string name;
+    public string fur_color;
+    public string breed;
+
+    public Dog(int age,double weight,string name,string fur_color,string breed) 
+    {
+        this.age = age;
+        this.weight = weight;
+        this.name = name;
+        this.fur_color = fur_color;
+        this.breed = breed;
+    }
+    public void bark()
+    {
+        Console.WriteLine("\nWoof! Woof!");
+    }
+    public void rename(string new_name)
+    {
+        this.name=new_name;
+    }
+    public void eat(double food)
+    {
+        this.weight += food;
+    }
+}
+class Lab12B
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("You are about to create a dog.");
+        Console.Write("How old is the dog: ");
+        int age =int.Parse(Console.ReadLine());
+        Console.Write("How much does the dog weigh: ");
+        double weight =int.Parse(Console.ReadLine());
+        Console.Write("What is the dog’s name: ");
+        string name =Console.ReadLine();
+        Console.Write("What color is the dog: ");
+        string fur_color =Console.ReadLine();
+        Console.Write("What breed is the dog: ");
+        string breed =Console.ReadLine();
+        Dog d1=new Dog(age,weight,name,fur_color,breed);
+
+        Console.WriteLine("\n"+d1.name+" is a "+d1.age+" year old "+d1.fur_color+" "+d1.breed+" that weighs "+d1.weight+" lbs.");
+        d1.bark();
+        Console.Write(d1.name+" is hungry, how much should he eat: ");
+        double food=double.Parse(Console.ReadLine());
+        d1.eat(food);
+        Console.Write(d1.name+" isn’t a very good name. What should they be renamed to: ");
+        string new_name=Console.ReadLine();
+        d1.rename(new_name);
+        Console.WriteLine("\n" + d1.name + " is a " + d1.age + " year old " + d1.fur_color + " " + d1.breed + " that weighs " + d1.weight + " lbs.");
+
+
+    }
+}
